e7fe6dc0487fa6ca3156539f0ceacb804a3dc03a
RodrigoBonif <digoboni22@gmail.com>
2022-01-25 20:25:58 -0300

    Conteudo do Site
    imagens e formatação das paginas home e comunidades

--- src/components/Comunidades.vue
+++ src/components/Comunidades.vue
@@ -2,38 +2,36 @@
     <div class="corpo">
         <div class="menu-minhas-comunidades">
             <h1 class="text-white">Minhas Comunidades:</h1>
-            <div class="box-conteudo-comunidades"></div>
-            <div class="box-conteudo-comunidades"></div>
-            <div class="box-conteudo-comunidades"></div>
-            <div class="box-conteudo-comunidades"></div>
-            <div class="box-adicionar-comunidades"></div>
+            <div class="container-conteudo-comunidades">
+                <div class="box-conteudo-comunidades">
+                    <img class="imagem-comunidades" src="../../public/images/paginaComunidades/DarkSouls.jpg" alt="">
+                </div>
+                <div class="box-adicionar-comunidades"><h1 class="criar-comunidades">+</h1></div>
+            </div>
         </div>
         <hr class="linha-comunidades">
         <div class="box-titulo-outras-comu">
             <h1 class="text-white">Outras Comunidades:</h1>
         </div>
         <div class="menu-comunidades">
-            <div class="box-conteudo-comunidades"></div>
-            <div class="box-conteudo-comunidades"></div>
-            <div class="box-conteudo-comunidades"></div>
-            <div class="box-conteudo-comunidades"></div>
-            <div class="box-conteudo-comunidades"></div>
-            <div class="box-conteudo-comunidades"></div>
-            <div class="box-conteudo-comunidades"></div>
-            <div class="box-conteudo-comunidades"></div>
-            <div class="box-conteudo-comunidades"></div>
-            <div class="box-conteudo-comunidades"></div>
-            <div class="box-conteudo-comunidades"></div>
-            <div class="box-conteudo-comunidades"></div>
+            <div class="box-conteudo-comunidades"><img class="imagem-comunidades" src="../../public/images/paginaComunidades/RocketLeague.png" alt=""></div>
+            <div class="box-conteudo-comunidades"><img class="imagem-comunidades" src="../../public/images/paginaComunidades/CounterStrike.jpg" alt=""></div>
+            <div class="box-conteudo-comunidades"><img class="imagem-comunidades" src="../../public/images/paginaComunidades/DeadByDaylight.jpg" alt=""></div>
+            <div class="box-conteudo-comunidades"><img class="imagem-comunidades" src="../../public/images/paginaComunidades/Valorant.png" alt=""></div>
+            <div class="box-conteudo-comunidades"><img class="imagem-comunidades" src="../../public/images/paginaComunidades/Phasmophobia.png" alt=""></div>
+            <div class="box-conteudo-comunidades"><img class="imagem-comunidades" src="../../public/images/paginaComunidades/GTA.jpg" alt=""></div>
+            <div class="box-conteudo-comunidades"><img class="imagem-comunidades" src="../../public/images/paginaComunidades/LeagueOfLegends.jpg" alt=""></div>
+            <div class="box-conteudo-comunidades"><img class="imagem-comunidades" src="../../public/images/paginaComunidades/Bloons.jpg" alt=""></div>
+            <div class="box-conteudo-comunidades"><img class="imagem-comunidades" src="../../public/images/paginaComunidades/PUBG.jpg" alt=""></div>
         </div>
         <div class="menu-lat-comunidades">
             <div class="box-titulo-comunidade-destaque">
                 <h1 class="text-destaques">Chats Destaques:</h1>
             </div>
-            <div class="box-comunidades-destaques"></div>
-            <div class="box-comunidades-destaques"></div>
-            <div class="box-comunidades-destaques"></div>
-            <div class="box-comunidades-destaques"></div>
+            <div class="box-comunidades-destaques"><h2 class="conteudo-chats-destaque">Vandal X Phantom</h2></div>
+            <div class="box-comunidades-destaques"><h2 class="conteudo-chats-destaque">FAQ de Dark Souls</h2></div>
+            <div class="box-comunidades-destaques"><h2 class="conteudo-chats-destaque">Mods para GTA 5</h2></div>
+            <div class="box-comunidades-destaques"><h2 class="conteudo-chats-destaque">Prints de Rocket League</h2></div>
         </div>
     </div>
 </template>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,57 +5,96 @@
         <div class="main">
             <div class="menu-lat">
                 <h1 class="titulo-menu">Comunidades Destaque:</h1>
-                <div class="box-menu"></div>
-                <div class="box-menu"></div>
-                <div class="box-menu"></div>
-                <div class="box-menu"></div>
-                <div class="box-menu"></div>
+                <div class="box-menu texto-comuni-destaque">Comunidade: Rocket League</div>
+                <div class="box-menu texto-comuni-destaque">Comunidade: Valorant</div>
+                <div class="box-menu texto-comuni-destaque">Comunidade: Counter Strike</div>
+                <div class="box-menu texto-comuni-destaque">Comunidade: GTA</div>
+                <div class="box-menu texto-comuni-destaque">Comunidade: Dark Souls</div>
             </div>
             <div class="container-home">
                 <div class="box-home">
-                    <div class="box-conteudo-home"></div>
+                    <div class="box-conteudo-home"><img class="imagem-home" src="../../public/images/paginaHome/video.png" alt=""></div>
+                    <h3 class="text-white text-conteudo-home">Achei esse mod de Gta muito legal e queria compartilhar com vocês</h3>
+                    <br>
+                    <p class="text-green text-filtro-conteudo">Filtros: GTA</p>
                 </div>
                 <div class="box-home">
-                    <div class="box-conteudo-home"></div>
+                    <div class="box-conteudo-home"><img class="imagem-home" src="../../public/images/paginaHome/pergunta.png" alt=""></div>
+                    <h3 class="text-white text-conteudo-home">Faz 3 horas que estou tentando matar esse boss e não consigo, alguem pode me ajudar?</h3>
+                    <br>
+                    <p class="text-green text-filtro-conteudo">Filtros: Dark Souls</p>
                 </div>
                 <div class="box-home">
-                    <div class="box-conteudo-home"></div>
+                    <div class="box-conteudo-home"><img class="imagem-home" src="../../public/images/paginaHome/video.png" alt=""></div>
+                    <h3 class="text-white text-conteudo-home">As melhores customizações para o seu carro favorito do Rocket</h3>
+                    <br>
+                    <p class="text-green text-filtro-conteudo">Filtros: Rocket League</p>
                 </div>
                 <div class="box-home">
-                    <div class="box-conteudo-home"></div>
+                    <div class="box-conteudo-home"><img class="imagem-home" src="../../public/images/paginaHome/pergunta.png" alt=""></div>
+                    <h3 class="text-white text-conteudo-home">Estou com uma duvida: Como faço para comprar Valorant Points com o boleto?</h3>
+                    <br>
+                    <p class="text-green text-filtro-conteudo">Filtros: Valorant</p>
                 </div>
                 <div class="box-home">
-                    <div class="box-conteudo-home"></div>
+                    <div class="box-conteudo-home"><img class="imagem-home" src="../../public/images/paginaHome/pergunta.png" alt=""></div>
+                    <h3 class="text-white text-conteudo-home">Alguém ai quer jogar umas ranqueadas comigo?, estou Diamante, adiciona ai DarkSun#4967</h3>
+                    <br>
+                    <p class="text-green text-filtro-conteudo">Filtros: Valorant</p>
                 </div>
                 <div class="box-home">
-                    <div class="box-conteudo-home"></div>
+                    <div class="box-conteudo-home"><img class="imagem-home" src="../../public/images/paginaHome/noticia.png" alt=""></div>
+                    <h3 class="text-white text-conteudo-home">As melhores smokes para o mapa da Mirage, apenas 64 tick</h3>
+                    <br>
+                    <p class="text-green text-filtro-conteudo">Filtros: Counter Strike</p>
                 </div>
                 <div class="box-home">
-                    <div class="box-conteudo-home"></div>
+                    <div class="box-conteudo-home"><img class="imagem-home" src="../../public/images/paginaHome/pergunta.png" alt=""></div>
+                    <h3 class="text-white text-conteudo-home">Como faço para conseguir o milagre lamina da luz sagrada?</h3>
+                    <br>
+                    <p class="text-green text-filtro-conteudo">Filtros: Dark Souls</p>
                 </div>
                 <div class="box-home">
-                    <div class="box-conteudo-home"></div>
+                    <div class="box-conteudo-home"><img class="imagem-home" src="../../public/images/paginaHome/noticia.png" alt=""></div>
+                    <h3 class="text-white text-conteudo-home">Modo de conseguir um milhão de reais fazendo o novo roubo ao cassino</h3>
+                    <br>
+                    <p class="text-green text-filtro-conteudo">Filtros: GTA</p>
                 </div>
                 <div class="box-home">
-                    <div class="box-conteudo-home"></div>
+                    <div class="box-conteudo-home"><img class="imagem-home" src="../../public/images/paginaHome/pergunta.png" alt=""></div>
+                    <h3 class="text-white text-conteudo-home">Qual que está mais forte no meta de hoje, a M4a4 ou a M4 silenciada?</h3>
+                    <br>
+                    <p class="text-green text-filtro-conteudo">Filtros: Counter Strike</p>
                 </div>
                 <div class="box-home">
-                    <div class="box-conteudo-home"></div>
+                    <div class="box-conteudo-home"><img class="imagem-home" src="../../public/images/paginaHome/pergunta.png" alt=""></div>
+                    <h3 class="text-white text-conteudo-home">Será que está valendo a pena comprar o passe novo do Rocket?</h3>
+                    <br>
+                    <p class="text-green text-filtro-conteudo">Filtros: Rocket League</p>
                 </div>
                 <div class="box-home">
-                    <div class="box-conteudo-home"></div>
+                    <div class="box-conteudo-home"><img class="imagem-home" src="../../public/images/paginaHome/video.png" alt=""></div>
+                    <h3 class="text-white text-conteudo-home">Novo trailer Da Agente Neon que saiu hoje</h3>
+                    <br>
+                    <p class="text-green text-filtro-conteudo">Filtros: Valorant</p>
                 </div>
                 <div class="box-home">
-                    <div class="box-conteudo-home"></div>
+                    <div class="box-conteudo-home"><img class="imagem-home" src="../../public/images/paginaHome/noticia.png" alt=""></div>
+                    <h3 class="text-white text-conteudo-home">Veja os proximos participantes do major 2022</h3>
+                    <br>
+                    <p class="text-green text-filtro-conteudo">Filtros: Counter Strike</p>
                 </div>
                 <div class="box-home">
-                    <div class="box-conteudo-home"></div>
+                    <div class="box-conteudo-home"><img class="imagem-home" src="../../public/images/paginaHome/pergunta.png" alt=""></div>
+                    <h3 class="text-white text-conteudo-home">Existe outra maneira de comprar os carros do passe, ou eu deveria comprá-lo?</h3>
+                    <br>
+                    <p class="text-green text-filtro-conteudo">Filtros: Rocket League</p>
                 </div>
                 <div class="box-home">
-                    <div class="box-conteudo-home"></div>
-                </div>
-                <div class="box-home">
-                    <div class="box-conteudo-home"></div>
+                    <div class="box-conteudo-home"><img class="imagem-home" src="../../public/images/paginaHome/noticia.png" alt=""></div>
+                    <h3 class="text-white text-conteudo-home">Novo patch de atualizações do GTA Online</h3>
+                    <br>
+                    <p class="text-green text-filtro-conteudo">Filtros: GTA</p>
                 </div>
             </div>
         </div>
